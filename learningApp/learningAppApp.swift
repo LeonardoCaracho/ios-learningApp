545e68b308ca136d82c6b4cd0fb24aa35c3c1b92
Leonardo Caracho <leonardo.caracho@partner.bmw.com.br>
2023-03-11 08:08:55 -0300

    Initial Commit

---
+++ learningApp/learningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningAppApp.swift
+//  learningApp
+//
+//  Created by Leonardo Caracho on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct learningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
