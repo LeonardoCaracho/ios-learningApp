bd54e7cb183cfc10363e101f4d73ea46f5761616
Leonardo Caracho <leonardo.caracho@partner.bmw.com.br>
2023-03-20 08:51:38 -0300

    - submit button in test view - highlight proper colors

--- learningApp/Views/TestView.swift
+++ learningApp/Views/TestView.swift
@@ -9,13 +9,82 @@ import SwiftUI
 
 struct TestView: View {
     @EnvironmentObject var model: ContentModel
+    @State var selectedAnswerIndex:Int?
+    @State var numCorrect = 0
+    @State var submitted = false
     
     var body: some View {
         if model.currentQuestion != nil {
-            VStack {
+            VStack(alignment: .leading) {
                 Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
                 
                 CodeTextView()
+                    .padding(.horizontal, 20)
+                
+                ScrollView {
+                    VStack {
+                        ForEach (0..<model.currentQuestion!.answers.count, id: \.self) { index in
+                            Button(action: {
+                                selectedAnswerIndex = index
+                            }) {
+                                ZStack {
+                                    
+                                    if submitted == false {
+                                        RectangleCard(
+                                            color: index == selectedAnswerIndex ? .gray : .white,
+                                            height: 48
+                                        )
+                                    } else {
+                                        if (index == selectedAnswerIndex &&
+                                            index == model.currentQuestion!.correctIndex) ||
+                                            index == model.currentQuestion!.correctIndex {
+                                            RectangleCard(
+                                                color: .green,
+                                                height: 48
+                                            )
+                                        }
+                                        else if index == selectedAnswerIndex &&
+                                                    index != model.currentQuestion!.correctIndex {
+                                            RectangleCard(
+                                                color: .red,
+                                                height: 48
+                                            )
+                                        } else {
+                                            RectangleCard(
+                                                color: .white,
+                                                height: 48
+                                            )
+                                        }
+                                    }
+                                    
+                                    
+                                    Text(model.currentQuestion!.answers[index])
+                                }
+                            }
+                            .disabled(submitted)
+                        }
+                    }
+                    .accentColor(.black)
+                    .padding()
+                }
+                
+                Button(action: {
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                        numCorrect += 1
+                    }
+                    submitted.toggle()
+                }) {
+                    ZStack {
+                        RectangleCard(color: .green, height: 48)
+                        
+                        Text("Submit")
+                            .bold()
+                    }
+                }
+                .accentColor(.white)
+                .padding()
+                .disabled(selectedAnswerIndex == nil)
             }
             .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
         } else {
